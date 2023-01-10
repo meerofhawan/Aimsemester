862c8c6eeb2af7d2567f85019574f9633d923490
muhammaduzair2000 <122357257+muhammaduzair2000@users.noreply.github.com>
2023-01-10 15:18:58 +0500

    Update projectgui.py

--- projectgui.py
+++ projectgui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from PIL import ImageTk, Image  
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
-
+# python
 
 class Application(tk.Tk):
     def __init__(self):
